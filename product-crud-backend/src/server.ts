ad632e19933dd43c4f2fa957833c133d5bc834af
Muhammad Ata <Muhammad_ata@outlook.com>
2021-03-25 21:56:46 +0300

    frontend boilerplate

--- product-crud-backend/src/server.ts
+++ product-crud-backend/src/server.ts
@@ -16,7 +16,7 @@ class Server {
       }
 
     private config(): void {
-        this.app.set('port', 3000);
+        this.app.set('port', 4000);
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
         this.app.use(cors());
